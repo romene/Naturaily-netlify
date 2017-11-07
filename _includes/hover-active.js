c08f9db462bee02740ad3ea602e6a0c96bfe3243
Unknown <mateusz.parzyk@gmail.com>
2017-11-07 09:00:01 +0100

    change sass

--- _includes/hover-active.js
+++ _includes/hover-active.js
@@ -2,12 +2,12 @@ var container, list;
 $("document").ready(function() {
   list = $('#hoverPanelsContainer');
   list.on("mouseenter", '.panel', function(element) {
-    let cont = element.target.dataset.target;
+    let target = element.target.dataset.target;
     $('#hoverPanelsContainer div.active').removeClass("active");
     $('#hoverPanelsContent div.active').removeClass("active");
     $(element.target).addClass('active');
-    $('#hoverPanelsContent #' + cont).addClass('active');
-    if (cont === undefined) {
+    $('#hoverPanelsContent #' + target).addClass('active');
+    if (target === undefined) {
       $('#hoverPanelsContent #elastic').addClass('active');
     }
   })
