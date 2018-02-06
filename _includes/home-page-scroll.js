4591a5aa64466bf38203ae49548258174d0935d6
Daniel Oskierko <daniel.oskierko@naturaily.com>
2018-02-06 10:30:11 +0100

    DRY js

--- _includes/home-page-scroll.js
+++ _includes/home-page-scroll.js
@@ -1,80 +1,60 @@
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true, duration: 600 });
 
 sr.reveal('[data-reveal-resources]', {
-  reset: true,
   delay: 400,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
   rotate: {x: 65},
 });
 
 sr.reveal('[data-reveal-technology]', {
-  reset: true,
   delay: 300,
-  duration: 600,
 });
 
 sr.reveal('[data-reveal-tools]', {
-  reset: true,
   origin: 'bottom',
   delay: 300,
-  duration: 600,
   distance: '150px',
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
   rotate: {x: 65},
 });
 
 sr.reveal('[data-reveal-results]', {
-  reset: true,
   origin: 'left',
   delay: 300,
-  duration: 600,
   distance: '100px',
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
   rotate: {x: 65},
 });
 
 sr.reveal('[data-reveal-competence]', {
-  reset: true,
   origin: 'right',
   delay: 300,
-  duration: 600,
   distance: '100px',
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
   rotate: {x: 65},
 });
 
 sr.reveal('#galleryFirst', {
-  reset: true,
   delay: 300,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
 
 sr.reveal('#gallerySecond', {
-  reset: true,
   delay: 400,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
 
 sr.reveal('#galleryThird', {
-  reset: true,
   delay: 500,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
 
 sr.reveal('#galleryFourth', {
-  reset: true,
   delay: 600,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
 
 sr.reveal('#galleryFifth', {
-  reset: true,
   delay: 700,
-  duration: 600,
   easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 });
