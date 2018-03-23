59079f36b36a8f58f8ab9a91c050cb63a52e96fc
Daniel Oskierko <daniel.oskierko@naturaily.com>
2018-03-23 15:54:30 +0100

    scroll function: enable - tak() and disable - nie()

--- _includes/block-scroll.js
+++ _includes/block-scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    $('#checkbox').on('click', function() {
-      $('html, body').css("overflow", function(_,val){
+      /*$('html, body').css("overflow", function(_,val){
         return val == "hidden" ? "visible" : "hidden";
       });
       $('html, body').css("position", function(_,val){
@@ -8,9 +8,31 @@ $(document).ready(function() {
       });
       $('html, body').css("height", function(_,val){
         return val == "100%" ? "auto" : "100%";
-      });
+      });*/
+      $("html, body").animate({ scrollTop: 0 }, "slow");
       $('#Smallchat, #scrollUp').css("visibility", function(_,val){
         return val == "hidden" ? "visible" : "hidden";
       });
    });
 });
+
+
+
+function tak() {
+  console.log("włączona, nie będziesz scrollował");
+  window.addEventListener("scroll", preventMotion, false);
+  window.addEventListener("touchmove", preventMotion, false);
+}
+
+function nie() {
+  console.log("wyłączona, możesz scrollować");
+  window.removeEventListener("scroll", preventMotion, false);
+  window.removeEventListener("touchmove", preventMotion, false);
+}
+
+function preventMotion(event) {
+  console.log("blokuje Ci scrolla :P");
+  window.scrollTo(0, 0);
+  event.preventDefault();
+  event.stopPropagation();
+}
