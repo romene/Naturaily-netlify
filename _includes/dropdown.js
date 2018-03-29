c80f9a6e0aa1524b41f9b473c212a6dfe7bab8f4
Daniel Oskierko <daniel.oskierko@naturaily.com>
2018-03-29 13:41:03 +0200

    change main carousel

--- _includes/dropdown.js
+++ _includes/dropdown.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-   $('#dropdown-toggle').on('click', function() {
-      $('#dropdown').css("display", function(_,val){
-        return val == "block" ? "none" : "block";
-      });
-   });
+$('#dropdown-toggle').on('click', function() {
+  $('#dropdown').css("display", function(_,val){
+    return val == "block" ? "none" : "block";
+  });
 });
--- _includes/main-carousel.js
+++ _includes/main-carousel.js
@@ -7,10 +7,24 @@ $('#maincarousel').slick({
   speed: 300
 });
 
-$(document).ready(function() {
-   $('#post1').on('click', function() {
-      $('.slick-list').css("height", function(_,val){
-        return val == "570px" ? "420px" : "570px";
-      });
-   });
+$('#post1').on('click', function() {
+  if ($(window).width() < 370) {
+
+    $('.slick-list').css("height", function(_,val){
+      return val == "570px" ? "420px" : "570px";
+    });
+
+  } else if ($(window).width() < 409) {
+
+    $('.slick-list').css("height", function(_,val){
+      return val == "480px" ? "380px" : "480px";
+    });
+
+  } else {
+
+    $('.slick-list').css("height", function(_,val){
+      return val == "420px" ? "340px" : "420px";
+    });
+
+  }
 });
