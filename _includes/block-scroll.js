ab5d55a9d2a3e276ccabb39f204a59b012004b1e
Daniel Oskierko <daniel.oskierko@naturaily.com>
2018-03-29 13:38:32 +0200

    fix mobile menu

--- _includes/block-scroll.js
+++ _includes/block-scroll.js
@@ -1,22 +1,29 @@
-$(document).ready(function() {
-   $('#checkbox').on('click', function() {
-
-      $('#Smallchat').css("visibility", function(_,val){
-        return val == "hidden" ? "visible" : "hidden";
-      });
-
-      if ($('.menu').hasClass('active')) {
-        $(".menu").removeClass('active');
-      } else {
-        $(".menu").addClass('active');
-      }
-   });
-});
-
 var didScroll,
     lastScrollTop = 0,
     delta = 5,
-    navbarHeight = $('#checkbox, [data-reveal-header]').outerHeight();
+    navbarHeight = $('#checkbox, #mobileSpan').outerHeight();
+
+$('#checkbox').on('click', function() {
+
+  $('#Smallchat').css("visibility", function(_,val){
+    return val == "hidden" ? "visible" : "hidden";
+  });
+
+  if ($('.menu').hasClass('active')) {
+
+    $(".menu").removeClass('active');
+
+  } else {
+
+    $(".menu").addClass('active');
+
+  }
+});
+
+$('#mobileSpan').click(function(){
+	$(this).toggleClass('open');
+  $('#checkbox').click();
+});
 
 $(window).scroll(function(event){
   didScroll = true;
@@ -36,12 +43,14 @@ function hasScrolled() {
   return;
 
   if (st > lastScrollTop && st > navbarHeight){
-    $('#checkbox, [data-reveal-header]').removeClass('nav-down').addClass('nav-up');
+    $('#checkbox, #mobileSpan').removeClass('nav-down').addClass('nav-up');
     $(".menu").removeClass('active');
+    $("#mobileSpan").removeClass('open');
   } else {
     if(st + $(window).height() < $(document).height()) {
-      $('#checkbox, [data-reveal-header]').removeClass('nav-up').addClass('nav-down');
+      $('#checkbox, #mobileSpan').removeClass('nav-up').addClass('nav-down');
       $(".menu").removeClass('active');
+      $("#mobileSpan").removeClass('open');
     }
   }
 
