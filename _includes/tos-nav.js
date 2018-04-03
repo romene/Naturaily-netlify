b325e347925f297e63bfc08f2ab35a5877d82d11
Daniel Oskierko <daniel.oskierko@naturaily.com>
2018-04-03 10:15:28 +0200

    tos navigation

--- _includes/tos-nav.js
+++ _includes/tos-nav.js
@@ -1,33 +1,40 @@
-$('[data-target-terms]').on('click', function () {
-  $('.tos-nav-list > li').removeClass('active');
-  $('[data-target-terms]').addClass('active');
-  $('html, body').animate({
-    scrollTop: $("#terms").offset().top
-  }, 1000);
-});
-
-$('[data-target-materials]').on('click', function () {
-  $('.tos-nav-list > li').removeClass('active');
-  $('[data-target-materials]').addClass('active');
-  $('html, body').animate({
-    scrollTop: $("#materials").offset().top
-  }, 1000);
-});
-
-$('[data-target-analytics]').on('click', function () {
-  $('.tos-nav-list > li').removeClass('active');
-  $('[data-target-analytics]').addClass('active');
-  $('html, body').animate({
-    scrollTop: $("#analytics").offset().top
-  }, 1000);
-});
-
-$('[data-target-cookies]').on('click', function () {
-  $('.tos-nav-list > li').removeClass('active');
-  $('[data-target-cookies]').addClass('active');
-  $('html, body').animate({
-    scrollTop: $("#cookies").offset().top
-  }, 1000);
-});
-
 $("#tosNav").stick_in_parent({ offset_top: 100 });
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+            $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#tosNav a').each(function () {
+        var currLink = $(this),
+            refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#tosNav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
