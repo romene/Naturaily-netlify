70f7af6cc92a7062e2701736b466de97d053ee46
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-04-25 15:25:08 +0200

    form testing

--- _includes/modal.js
+++ _includes/modal.js
@@ -16,3 +16,25 @@ $(function() {
     $('html, body').css('overflow', '');
   });
 });
+
+
+$(function() {
+
+  const entry = $('[data-job-modal-trigger]'),
+        exit = $("data-job-modal-close, [data-job-modal]"),
+        container = $("[data-job-modal]"),
+        form = $("[data-job-modal-form]");
+
+  entry.click(function() {
+    container.css('visibility', 'visible');
+    form.css('transform', 'translate(-50%, -50%)');
+    $('html, body').css('cssText', 'overflow: hidden !important');
+  });
+
+  exit.click(function() {
+    container.css('visibility', 'hidden');
+    form.css('transform', 'translate(-50%, -200%)');
+    $('html, body').css('overflow', '');
+  });
+
+});
