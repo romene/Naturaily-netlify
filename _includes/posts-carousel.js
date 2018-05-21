607df734364fa8dac03fd000fac3b42cd64bddbd
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-05-21 17:22:57 +0200

    adding ror tech page

---
+++ _includes/posts-carousel.js
@@ -0,0 +1,9 @@
+$('#postsCarousel').slick({
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: true,
+  dots: false,
+  speed: 300,
+  arrows: false
+});
---
+++ _includes/tech-carousel.js
@@ -0,0 +1,9 @@
+$('#techCarousel').slick({
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: true,
+  dots: false,
+  speed: 300,
+  arrows: false
+});
