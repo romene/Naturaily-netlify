6b44cac314b9e11c6bde255094d8d96d9d7e6341
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-05-23 12:22:31 +0200

    carousel improvement

--- _includes/posts-carousel.js
+++ _includes/posts-carousel.js
@@ -1,9 +1,23 @@
 $('#postsCarousel').slick({
-  slidesToShow: 3,
+  slidesToShow: 5,
   autoplay: true,
   autoplaySpeed: 4000,
   pauseOnHover: true,
   dots: false,
   speed: 300,
-  arrows: true
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
