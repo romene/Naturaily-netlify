b57081ffeb4ce7226f626e2720a909de50843691
Daniel Oskierko <daniel.oskierko@naturaily.com>
2019-02-19 19:50:55 +0100

    improvements

--- _includes/tos-nav.js
+++ _includes/tos-nav.js
@@ -1,40 +1,62 @@
-$("#tosNav").stick_in_parent({ offset_top: 100 });
+const $secondSection = $('.tos-content div:nth-child(2)');
+const $thirdSection = $('.tos-content div:nth-child(3)');
+const $fourthSection = $('.tos-content div:nth-child(4)');
+const $fifthSection = $('.tos-content div:nth-child(5)');
+const $firstNavOption = $('#tosNav li:nth-child(1) a');
+const $secondNavOption = $('#tosNav li:nth-child(2) a');
+const $thirdNavOption = $('#tosNav li:nth-child(3) a');
+const $fourthNavOption = $('#tosNav li:nth-child(4) a');
+const $fifthNavOption = $('#tosNav li:nth-child(5) a');
+
+$('#tosNav').stick_in_parent({ offset_top: 100 });
 
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
+  checkCurrentSection();
+  $(document).on('scroll', checkCurrentSection);
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off('scroll');
 
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+    $('a').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
 
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-            $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+    var target = this.hash,
+        menu = target;
+        $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top + 2
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+      $(document).on('scroll', checkCurrentSection);
     });
+  });
 });
 
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#tosNav a').each(function () {
-        var currLink = $(this),
-            refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#tosNav ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
+function checkCurrentSection() {
+  const $scrollPosition = $(window).scrollTop();
+
+  if ($scrollPosition < $secondSection.offset().top) {
+    removeActiveClasses()
+    $firstNavOption.addClass('active');
+  } else if ($scrollPosition < $thirdSection.offset().top) {
+    removeActiveClasses()
+    $secondNavOption.addClass('active');
+  } else if ($scrollPosition < $fourthSection.offset().top) {
+    removeActiveClasses()
+    $thirdNavOption.addClass('active');
+  } else if ($scrollPosition < $fifthSection.offset().top) {
+    removeActiveClasses()
+    $fourthNavOption.addClass('active');
+  } else {
+    removeActiveClasses()
+    $fifthNavOption.addClass('active');
+  }
+}
+
+function removeActiveClasses() {
+  $('#tosNav a').each(function () {
+    $(this).removeClass("active");
+  });
 }
