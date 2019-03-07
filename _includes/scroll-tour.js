8edfe116843ada2374a3ffa023331f878262a215
Daniel Oskierko <daniel.oskierko@naturaily.com>
2019-03-07 12:18:34 +0100

    add sticky top-bar

--- _includes/scroll-tour.js
+++ _includes/scroll-tour.js
@@ -12,16 +12,87 @@ $(document).ready(function(){
 
         return false;
       });
+
     } else {
-      $('a[href^="#"]').click(function() {
+      const $secondSection = $('#videos');
+      const $thirdSection = $('#rhythm');
+      const $fourthSection = $('#hits');
+      const $fifthSection = $('#cooperation');
+      const $sixthSection = $('#band');
+      const $sevenSection = $('#dates');
+      const $eighthSection = $('#photos');
+      const $firstNavOption = $('.tour-top-bar li:nth-child(2) a');
+      const $secondNavOption = $('.tour-top-bar li:nth-child(3) a');
+      const $thirdNavOption = $('.tour-top-bar li:nth-child(4) a');
+      const $fourthNavOption = $('.tour-top-bar li:nth-child(5) a');
+      const $fifthNavOption = $('.tour-top-bar li:nth-child(6) a');
+      const $sixthNavOption = $('.tour-top-bar li:nth-child(7) a');
+      const $sevenNavOption = $('.tour-top-bar li:nth-child(8) a');
+      const $eighthNavOption = $('.tour-top-bar li:nth-child(9) a');
+
+      checkCurrentSection();
+      $(document).on('scroll', checkCurrentSection);
+
+      $('a[href^="#"]').on('click', function () {
         const the_id = $(this).attr("href");
 
-        $('html, body').animate({
-          scrollTop: ($(the_id).offset().top - 80)
-        }, 'slow');
+        $('a').each(function () {
+          $(this).removeClass('active');
+        })
+        $(this).addClass('active');
 
-        return false;
+        if (the_id !== '#banner') {
+          $('html, body').animate({
+            scrollTop: ($(the_id).offset().top - 130)
+          }, 'slow');
+        } else {
+          $('html, body').animate({
+            scrollTop: 0
+          }, 'slow');
+        }
       });
+
+      function checkCurrentSection() {
+        const $scrollPosition = $(window).scrollTop();
+
+        if ($(this).scrollTop() == 0) {
+          $('.tour-menu').removeClass('fixed');
+        } else {
+          $('.tour-menu').addClass('fixed');
+        }
+
+        if ($scrollPosition < $secondSection.offset().top - 150) {
+          removeActiveClasses();
+          $firstNavOption.addClass('active');
+        } else if ($scrollPosition < $thirdSection.offset().top - 150) {
+          removeActiveClasses();
+          $secondNavOption.addClass('active');
+        } else if ($scrollPosition < $fourthSection.offset().top - 150) {
+          removeActiveClasses();
+          $thirdNavOption.addClass('active');
+        } else if ($scrollPosition < $fifthSection.offset().top - 200) {
+          removeActiveClasses();
+          $fourthNavOption.addClass('active');
+        } else if ($scrollPosition < $sixthSection.offset().top - 150) {
+          removeActiveClasses();
+          $fifthNavOption.addClass('active');
+        } else if ($scrollPosition < $sevenSection.offset().top - 150) {
+          removeActiveClasses();
+          $sixthNavOption.addClass('active');
+        } else if ($scrollPosition < $eighthSection.offset().top - 150) {
+          removeActiveClasses();
+          $sevenNavOption.addClass('active');
+        } else {
+          removeActiveClasses();
+          $eighthNavOption.addClass('active');
+        }
+      }
+
+      function removeActiveClasses() {
+        $('.tour-top-bar a').each(function () {
+          $(this).removeClass("active");
+        });
+      }
     }
   }
 
@@ -85,6 +156,7 @@ $(document).ready(function(){
   window.onresize = function() {
     width = document.documentElement.clientWidth;
     $('a[href^="#"]').off('click');
+    $('a[href^="#"]').off('scroll');
     resizeForm();
     scrollToElement();
   }
